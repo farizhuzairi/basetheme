ab7f5a69d32d1d3a21486361f6e4fe5377369dd5
Fariz Huzairi <farizhuzairi@gmail.com>
2025-02-16 06:29:05 +0700

    updated

--- resources/views/basetheme/features/content/skull.blade.php
+++ resources/views/basetheme/features/content/skull.blade.php
@@ -7,12 +7,12 @@
     ])
 }}>
     <div class="flex flex-col justify-center items-center mb-12">
-        <h2 class="font-title font-semibold text-lg text-primary leading-5">{{ $subject }}</h2>
+        <h2 class="font-title font-semibold text-lg text-primary dark:text-c-text-white leading-5">{{ $subject }}</h2>
         @if($introduction)<p class="text-sm font-light">{{ $introduction }}</p>@endif
     </div>
     <div class="grid grid-cols-1 md:grid-cols-2 gap-5 md:gap-8">
         @for($x=0; $x < 4; $x++)
-        <div class="border border-c-border rounded shadow bg-white hover:bg-c-theme dark:bg-c-light dark:hover:bg-c-theme text-c-text hover:text-c-text-white hover:scale-105 hover:-translate-y-3 transition-all group">
+        <div class="border border-c-border rounded shadow bg-white hover:bg-c-theme dark:hover:bg-c-theme text-c-text hover:text-c-text-white hover:scale-105 hover:-translate-y-3 transition-all group">
             <div class="h-full border-b-2 border-primary flex flex-row gap-5 px-3 py-5 rounded-b group-hover:border-b-4 transition-all">
                 <div class="flex items-center text-3xl group-hover:text-warning"><span class="hascha-star"></span></div>
                 <div>
