351eb25769bef8ed7574f37419945741767b563b
Fariz Huzairi <farizhuzairi@gmail.com>
2025-02-16 05:20:59 +0700

    updated

--- resources/views/basetheme/features/content/skull.blade.php
+++ resources/views/basetheme/features/content/skull.blade.php
@@ -9,10 +9,12 @@
     <h2 class="font-title font-medium text-lg flex justify-center mb-12">{{ $subject }}</h2>
     <div class="grid grid-cols-2 gap-8">
         @for($x=0; $x < 4; $x++)
-        <div class="border border-c-border rounded-lg bg-white hover:bg-c-theme text-c-text hover:text-c-text-white transition-all px-3 py-5 flex flex-row gap-5 group">
-            <div class="flex items-center text-3xl group-hover:text-warning"><span class="hascha-star"></span></div>
-            <div>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure ea vitae culpa odit placeat. Sapiente accusantium, cum minima nostrum voluptatem sed. Labore.
+        <div class="border border-c-border rounded-lg bg-white hover:bg-c-theme text-c-text hover:text-c-text-white transition-all px-3 py-5 group">
+            <div class="border-b-3 border-c-theme flex flex-row gap-5">
+                <div class="flex items-center text-3xl group-hover:text-warning"><span class="hascha-star"></span></div>
+                <div>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure ea vitae culpa odit placeat. Sapiente accusantium, cum minima nostrum voluptatem sed. Labore.
+                </div>
             </div>
         </div>
         @endfor
